a47841c62a7aa1b71ec1a408a3a3f1adf68030d3
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2024-10-06 21:47:26 +0530

    update url backend

--- frontend/src/components/chatbot/Chatbot.js
+++ frontend/src/components/chatbot/Chatbot.js
@@ -44,7 +44,7 @@ const Chatbot = () => {
     setLoading(true); // Start loading
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/ask', {
+      const response = await fetch('https://chatbot-rx1l.onrender.com/ask', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ question: message }),
